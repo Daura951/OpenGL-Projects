109ffc536eb5b1c9efab910eebb6cdb2c6138b47
Daura951 <56042228+Daura951@users.noreply.github.com>
2024-06-14 17:03:58 -0400

    Add files via upload

---
+++ OpenGL Code/OpenGL Blending.cpp
@@ -0,0 +1,399 @@
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <math.h>
+#include <stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <Shaders/Shader.h>
+#include <Camera/camera.h>
+
+#include <vector>
+#include <map>
+
+using namespace std;
+
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height);
+void inputCallback(GLFWwindow *window);
+void mouseCallback(GLFWwindow *window, double xPos, double yPos);
+void scrollWheelCallback(GLFWwindow *window, double xOffset, double yOffset);
+unsigned int loadTexture2D(const char *filePath);
+
+float cubeVertices[] = {
+    // positions          // texture Coords
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
+float planeVertices[] = {
+    // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+    5.0f, -0.5f, 5.0f, 2.0f, 0.0f,
+    -5.0f, -0.5f, 5.0f, 0.0f, 0.0f,
+    -5.0f, -0.5f, -5.0f, 0.0f, 2.0f,
+
+    5.0f, -0.5f, 5.0f, 2.0f, 0.0f,
+    -5.0f, -0.5f, -5.0f, 0.0f, 2.0f,
+    5.0f, -0.5f, -5.0f, 2.0f, 2.0f};
+
+float windowVerticies[] = {
+    // positions         // texture Coords (swapped y coordinates because texture is flipped upside down)
+    0.0f, 0.5f, 0.0f, 0.0f, 0.0f,
+    0.0f, -0.5f, 0.0f, 0.0f, 1.0f,
+    1.0f, -0.5f, 0.0f, 1.0f, 1.0f,
+
+    0.0f, 0.5f, 0.0f, 0.0f, 0.0f,
+    1.0f, -0.5f, 0.0f, 1.0f, 1.0f,
+    1.0f, 0.5f, 0.0f, 1.0f, 0.0f};
+
+vector<glm::vec3> windowPostions;
+
+float deltaTime = 0.0f, lastFrame = 0.0f;
+float lastMouseX = 400, lastMouseY = 400;
+
+Camera camera(glm::vec3(0, 0, -3));
+
+bool firstMouse = true;
+
+int main()
+{
+
+    //----------------------Initialize GLFW and GLAD--------------------
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 800, "Hello Window!", NULL, NULL);
+
+    if (window == NULL)
+    {
+        cout << "Failed to create window!" << endl;
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to start GLAD" << endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 800);
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouseCallback);
+
+    glfwSetScrollCallback(window, scrollWheelCallback);
+
+    //-----------------Positions------------------------------------------
+
+    windowPostions.push_back(glm::vec3(-1.5f, 0.0f, -0.48f));
+    windowPostions.push_back(glm::vec3(1.5f, 0.0f, 0.51f));
+    windowPostions.push_back(glm::vec3(0.0f, 0.0f, 0.7f));
+    windowPostions.push_back(glm::vec3(-0.3f, 0.0f, -2.3f));
+    windowPostions.push_back(glm::vec3(0.5f, 0.0f, -0.6f));
+
+    //----------------Create and Bind VAOs, and VBOs----------------------
+
+    // Cube
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    // Plane
+    unsigned int planeVAO, planeVBO;
+
+    glGenVertexArrays(1, &planeVAO);
+    glGenBuffers(1, &planeVBO);
+
+    glBindVertexArray(planeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // window
+    unsigned int windowVAO, windowVBO;
+
+    glGenVertexArrays(1, &windowVAO);
+    glGenBuffers(1, &windowVBO);
+
+    glBindVertexArray(windowVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, windowVBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(windowVerticies), &windowVerticies, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //-----------Create and compile shaders---------------------------
+
+    Shader shaderProgram("../resources/shaders/depth_test/vertex.vs", "../resources/shaders/depth_test/fragment.fs");
+
+    //---------------------Texture Loading--------------
+
+    unsigned int cubeDiffuse = loadTexture2D("../resources/textures/crate.png");
+    unsigned int planeDiffuse = loadTexture2D("../resources/textures/renamon.png");
+    unsigned int grassDiffuse = loadTexture2D("../resources/textures/grass.png");
+    unsigned int windowDiffuse = loadTexture2D("../resources/textures/window.png");
+
+    glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture1"), 0);
+
+    // Enable depth testing and blending. We use this blend function since it gives us the effect we want
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    //----------------Render Loop-----------------------
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float curFrame = glfwGetTime();
+        deltaTime = curFrame - lastFrame;
+        lastFrame = curFrame;
+
+        glm::mat4 modelMat = glm::mat4(1.0f);
+        glm::mat4 viewMat = glm::mat4(1.0f);
+        glm::mat4 projectionMat = glm::mat4(1.0f);
+
+        glClearColor(0.1, 0.1, 0.1, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        viewMat = camera.GetViewMatrix();
+        projectionMat = glm::perspective(glm::radians(camera.Zoom), 800.0f / 800.0f, 0.1f, 100.0f);
+
+        shaderProgram.use();
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "viewMat"), 1, GL_FALSE, glm::value_ptr(viewMat));
+
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "projectionMat"), 1, GL_FALSE, glm::value_ptr(projectionMat));
+
+        // PLANE
+        glActiveTexture(GL_TEXTURE0);
+        glBindVertexArray(planeVAO);
+        glBindTexture(GL_TEXTURE_2D, planeDiffuse);
+
+        modelMat = glm::mat4(1.0f);
+        modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat));
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // CONTAINERS
+
+        glBindVertexArray(VAO);
+        glBindTexture(GL_TEXTURE_2D, cubeDiffuse);
+
+        modelMat = glm::mat4(1.0f);
+        modelMat = glm::translate(modelMat, glm::vec3(-1.0f, 0.01f, -1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        modelMat = glm::mat4(1.0f);
+        modelMat = glm::translate(modelMat, glm::vec3(2.0f, 0.01f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glBindVertexArray(windowVAO);
+        glBindTexture(GL_TEXTURE_2D, windowDiffuse);
+
+        // Since the depth test can override fragments, we want to render the farthest windows first
+        // Shove all positions into a map where key is the vector distance from the camera
+        map<float, glm::vec3> sortedWindowPositions;
+        for (int i = 0; i < windowPostions.size(); i++)
+        {
+            float distance = glm::length(camera.Position - windowPostions[i]);
+            sortedWindowPositions[distance] = windowPostions[i];
+        }
+
+        // Now we want to render farthest first so we iterate in reverse order
+        for (map<float, glm::vec3>::reverse_iterator itr = sortedWindowPositions.rbegin(); itr != sortedWindowPositions.rend(); ++itr)
+        {
+            modelMat = glm::mat4(1.0f);
+            modelMat = glm::translate(modelMat, itr->second);
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat));
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
+
+        // for (int i = 0; i < vegetation.size(); i++)
+        // {
+        //     modelMat = glm::mat4(1.0f);
+        //     modelMat = glm::translate(modelMat, vegetation[i]);
+        //     glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat));
+        //     glDrawArrays(GL_TRIANGLES, 0, 6);
+        // }
+
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        inputCallback(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    //--------Clear everything out-----------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &planeVAO);
+    glDeleteBuffers(1, &planeVBO);
+    glDeleteVertexArrays(1, &windowVAO);
+    glDeleteBuffers(1, &windowVBO);
+    glDeleteProgram(shaderProgram.ID);
+
+    glfwTerminate();
+    return 0;
+}
+
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void inputCallback(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    const float cameraSpeed = 2.5f * deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+}
+
+void mouseCallback(GLFWwindow *window, double xPos, double yPos)
+{
+
+    if (firstMouse)
+    {
+        firstMouse = false;
+        lastMouseX = xPos;
+        lastMouseY = yPos;
+    }
+
+    float xOffset = xPos - lastMouseX;
+    float yOffset = yPos - lastMouseY;
+
+    lastMouseX = xPos;
+    lastMouseY = yPos;
+
+    camera.ProcessMouseMovement(xOffset, yOffset);
+}
+
+void scrollWheelCallback(GLFWwindow *window, double xOffset, double yOffset)
+{
+    camera.ProcessMouseScroll(yOffset);
+}
+
+unsigned int loadTexture2D(char const *filePath)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, numOfChannels;
+    unsigned char *imageData = stbi_load(filePath, &width, &height, &numOfChannels, 0);
+    if (imageData)
+    {
+        GLenum format;
+        if (numOfChannels == 1)
+            format = GL_RED;
+        else if (numOfChannels == 3)
+            format = GL_RGB;
+        else if (numOfChannels == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, imageData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(imageData);
+    }
+    else
+    {
+        cout << "Texture failed to load at file path: " << filePath << endl;
+        stbi_image_free(imageData);
+    }
+
+    return textureID;
+}
